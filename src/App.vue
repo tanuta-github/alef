56f97aeae26d8ec39e7671de406a87ed7fbd7b75
Tany <tany-egorenko@yandex.ru>
2022-03-10 14:17:32 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <AppHeader />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font: 12px/1 "Open Sans", sans-serif;
 }
+
+.container {
+  max-width: 1390px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header class="header">
+        <div class="container">
+            <a href="#" class="header__logo">LOGO</a>
+            <div class="header__links">
+                <a href="#"><img src="@/assets/person.svg" alt="" /></a>
+                <a href="#"><img src="@/assets/favorite.svg" alt="" /></a>
+                <a href="#"><img src="@/assets/bag.svg" alt="" /></a>
+                <button v-on:click="menuOpen = true" class="icon-menu"><img src="@/assets/menu.svg" alt="" /></button>
+            </div>
+        </div>    
+    </header>
+</template>
+<script>
+export default {
+  name: 'AppHeader',
+  data: () => {
+      return {
+        menuOpen: false
+      }
+  }
+   
+}
+</script>
+<style lang="scss">
+
+@import '@/assets/common.scss';
+
+.header {
+    padding: 10px 0;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
+    &__logo {
+        color: $color-black;
+        line-height: 1;
+        font-size: 22px;
+        font-weight: 700;
+        text-decoration: none;
+    }
+    &__links {
+        float: right;
+        display: flex;
+        a {
+           margin: 0 10px; 
+        }
+        .icon-menu {
+            display: none;
+            background: none;
+            border: none;
+            margin-left: 10px;
+            cursor: pointer;
+            @include respond-to(mobile) { 
+                display: block;
+            }
+        }
+    }
+
+}
+</style>
